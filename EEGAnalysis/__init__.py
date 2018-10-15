e0a3e4fc543142deaa2ab97023da2e8b521dfdfc
zane muir <zanemuir1995@gmail.com>
2018-10-15 14:59:27 +0800

    threshold detection

--- EEGAnalysis/__init__.py
+++ EEGAnalysis/__init__.py
@@ -1,4 +1,10 @@
-__all__ = ["container"]
+"""
+EEGAnalysis module
+
+author: Yizhan Miao
+email: yzmiao@protonmail.com
+last update: Oct 15 2018
+"""
 
 from .container import CompactDataContainer, SplitDataContainer
 
@@ -33,3 +39,39 @@ def loadsplitdata(sgchdir, chidx, fs, markername="grating", marker_bias=None, _i
     # the same for 10, 10-1, 10-2, 10-3
     """
     return SplitDataContainer(sgchdir, chidx, fs, marker_bias, markername, _import_date, _roi_head)
+
+
+##### temp #####
+import numpy as np
+
+def group_consecutive(a, gap=1, killhead=True):
+    ''' group consecutive numbers in an array
+        modified from https://zhuanlan.zhihu.com/p/29558169'''
+    
+    if killhead and a[0] == 0:
+        skip = np.where(np.diff(a) > gap)[0][0]
+        return np.split(a, np.where(np.diff(a)[skip:] > gap)[0] + skip + 1)[1:]
+    else:
+        return np.split(a, np.where(np.diff(a) > gap)[0] + 1)
+    
+def detect_thresh(data, thresh, gap=1, datadown=False):
+    """detect datarise or datadown index by threshold
+    
+    Syntax: Idx = detect_thresh(data, thresh, gap, datarise)
+    
+    Key Arguments:
+    data     -- (np.array) 1D data array
+    thresh   -- (number) threshold for data
+    gap      -- (int) consecutive gap [default: 1]
+    datadown -- (bool) check for datadown (True) or datarise (False)
+                [default: False]
+    
+    Return:
+    Idx      -- (np.array) indices of the thresh points
+    
+    """
+    if not datadown:  # i.e. data rise
+        spike = group_consecutive(np.where(data > thresh)[0], gap=gap)
+    else:  # i.e. data down
+        spike = group_consecutive(np.where(data < thresh)[0], gap=gap)
+    return np.array([item[0] for item in spike])
